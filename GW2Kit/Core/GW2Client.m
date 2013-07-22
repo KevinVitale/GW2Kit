63e7aa7023045b1cc4ae51a5741a6ecff4ef90b6
Kevin Vitale <kevinvitale@gmail.com>
2013-07-22 00:23:09 -0400

    Adding 'Guild' & 'Maps' folders

--- GW2Kit/Core/GW2Client.m
+++ GW2Kit/Core/GW2Client.m
@@ -84,113 +84,7 @@
     return self;
 }
 
-#pragma mark - Resource Names
-
-
-- (void)itemsWithCompletion:(void (^)(NSError *, NSArray *))completion {
-    void (^finalCompletion)(NSError *, NSArray *) = ^ (NSError *error, NSArray *states) {
-        if(completion)
-            completion(error, states);
-    };
-    
-
-    RKObjectMapping *itemMapping = [RKObjectMapping mappingForClass:[NSMutableDictionary class]];
-    [itemMapping addAttributeMappingsFromArray:@[@"items"]];
-    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:itemMapping
-                                                                                       pathPattern:nil
-                                                                                           keyPath:nil
-                                                                                       statusCodes:nil];
-    
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://api.guildwars2.com/v1/items.json"]];
-    RKObjectRequestOperation *operation = [[RKObjectRequestOperation alloc] initWithRequest:request
-                                                                        responseDescriptors:@[responseDescriptor]];
-    
-    RKObjectRequestOperation *weak_op = operation;
-    [operation setCompletionBlock:^{
-        RKObjectRequestOperation *strong_op = weak_op;
-        finalCompletion(strong_op. error, [strong_op.mappingResult.array.lastObject valueForKey:@"items"]);
-    }];
-    [self enqueueObjectRequestOperation:operation];
-}
-
-- (void)itemDetailForID:(NSString *)itemID
-             completion:(void (^)(NSError *, GW2ItemDetail *))completion {
-    
-    void (^finalCompletion)(NSError *, GW2ItemDetail *) = ^ (NSError *error, GW2ItemDetail *itemDetail) {
-        if(completion)
-            completion(error, itemDetail);
-    };
-    
-    RKMapping *itemMapping    = [GW2ItemDetail mappingObject];
-    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:itemMapping
-                                                                                       pathPattern:nil
-                                                                                           keyPath:nil
-                                                                                       statusCodes:nil];
-    
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:[@"https://api.guildwars2.com/v1/item_details.json?item_id=" stringByAppendingString:itemID]]];
-    RKObjectRequestOperation *operation = [[RKObjectRequestOperation alloc] initWithRequest:request
-                                                                        responseDescriptors:@[responseDescriptor]];
-    
-
-    RKObjectRequestOperation *weak_op = operation;
-    [operation setCompletionBlock:^{
-        RKObjectRequestOperation *strong_op = weak_op;
-        finalCompletion(strong_op. error, strong_op.mappingResult.array.lastObject);
-    }];
-    [self enqueueObjectRequestOperation:operation];
-}
-
-
-
-
-- (void)recipesWithCompletion:(void (^)(NSError *, NSArray *))completion {
-    void (^finalCompletion)(NSError *, NSArray *) = ^ (NSError *error, NSArray *states) {
-        if(completion)
-            completion(error, states);
-    };
-    
-    RKObjectMapping *itemMapping = [RKObjectMapping mappingForClass:[NSMutableDictionary class]];
-    [itemMapping addAttributeMappingsFromArray:@[@"recipes"]];
-    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:itemMapping
-                                                                                       pathPattern:nil
-                                                                                           keyPath:nil
-                                                                                       statusCodes:nil];
-    
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://api.guildwars2.com/v1/recipes.json"]];
-    RKObjectRequestOperation *operation = [[RKObjectRequestOperation alloc] initWithRequest:request
-                                                                        responseDescriptors:@[responseDescriptor]];
-    
-    RKObjectRequestOperation *weak_op = operation;
-    [operation setCompletionBlock:^{
-        RKObjectRequestOperation *strong_op = weak_op;
-        finalCompletion(strong_op. error, [strong_op.mappingResult.array.lastObject valueForKey:@"recipes"]);
-    }];
-    [self enqueueObjectRequestOperation:operation];
-}
-
-- (void)recipeDetailForID:(NSString *)recipeID
-               parameters:(NSDictionary *)parameters
-               completion:(void (^)(NSError *, GW2RecipeDetail *))completion {
-    void (^finalCompletion)(NSError *, GW2RecipeDetail *) = ^ (NSError *error, GW2RecipeDetail *recipeDetail) {
-        if(completion)
-            completion(error, recipeDetail);
-    };
-    
-    NSMutableDictionary *params = [NSMutableDictionary new];
-    [params addEntriesFromDictionary:parameters];
-    if(recipeID)
-        params[@"recipe_id"] = recipeID;
-    
-    [self getObjectsAtPath:@"/v1/recipe_details.json"
-                parameters:params
-                   success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
-                       finalCompletion(nil, mappingResult.array.lastObject);
-                   }
-                   failure:^(RKObjectRequestOperation *operation, NSError *error) {
-                       finalCompletion(error, nil);
-                   }];
-}
-
+#pragma mark - IN PROGRESS
 - (void)buildWithCompletion:(void (^)(NSError *, GW2Build *))completion {
     void (^finalCompletion)(NSError *, GW2Build *) = ^ (NSError *error, GW2Build *build) {
         if(completion)
@@ -241,6 +135,7 @@
                    }];
 }
 
+#pragma mark - Singleton
 + (GW2Client *)sharedClient {
     static dispatch_once_t onceToken;
     static GW2Client *instance = nil;
