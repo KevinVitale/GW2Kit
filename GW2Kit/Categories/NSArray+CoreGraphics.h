e7f6c2d98c107bfa5bd3f7961fb3385ee794810b
Kevin Vitale <kevinvitale@gmail.com>
2014-01-05 00:54:45 -0500

    Added 'NSArray+CoreGraphics' category

---
+++ GW2Kit/Categories/NSArray+CoreGraphics.h
@@ -0,0 +1,28 @@
+//
+//  NSArray+CoreGraphics.h
+//  GW2Kit
+//
+//  Created by Kevin Vitale on 1/5/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (CoreGraphics)
+/*	Create a CGPoint from an NSArray.
+ *
+ *	The array format:
+ *		@[ @20.1, @5.5 ]
+ *
+ *	converts to the point:
+ *		{ 20.1, 5.5 }
+ *
+ *	@param precision The decimal precision for both coordinate values.
+ *
+ *	@return A CGPoint (using 'double' values when compiled on 64-bit systems).
+ */
+- (CGPoint)point:(NSInteger)precision;
+
+- (CGSize)size:(NSInteger)precision;
+- (CGRect)rect:(NSInteger)precision;
+@end
