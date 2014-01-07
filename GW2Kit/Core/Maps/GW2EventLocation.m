02d99950741f5aa8bf0c9591c4da119f982013e9
Kevin Vitale <kevinvitale@gmail.com>
2014-01-06 22:07:13 -0500

    Restored 'pointsJSONTransformer" to GW2EventLocation.m

--- GW2Kit/Core/Maps/GW2EventLocation.m
+++ GW2Kit/Core/Maps/GW2EventLocation.m
@@ -7,11 +7,40 @@
 //
 
 #import "GW2EventLocation.h"
+#import "NSArray+CoreGraphics.h"
 #import "MTLValueTransformer+CoreGraphics.h"
 
 @implementation GW2EventLocation
 + (NSValueTransformer *)zRangeJSONTransformer {
     return MTLReversibleSizeTransformer(0);
+}
+
++ (NSValueTransformer *)pointsJSONTransformer {
+    return [MTLValueTransformer reversibleTransformerWithForwardBlock:^id (NSArray *pointsArray) {
+        NSMutableArray *mutableValueArray = [NSMutableArray arrayWithCapacity:pointsArray.count];
+        for(NSArray *pointArray in pointsArray) {
+            CGPoint point = [pointArray point:1];
+#if TARGET_OS_IPHONE
+            NSValue *pointValue = [NSValue valueWithCGPoint:point];
+#else
+            NSValue *pointValue = [NSValue valueWithPoint:point];
+#endif
+            [mutableValueArray addObject:pointValue];
+        }
+        return [mutableValueArray copy];
+    }
+                                                         reverseBlock:^id (NSArray *valueArray) {
+                                                             NSMutableArray *mutablePointsArray = [NSMutableArray arrayWithCapacity:valueArray.count];
+                                                             for(NSValue *pointValue in valueArray) {
+#if TARGET_OS_IPHONE
+                                                                 CGPoint point = [pointValue CGPointValue];
+#else
+                                                                 CGPoint point = (CGPoint)[pointValue pointValue];
+#endif
+                                                                 [mutablePointsArray addObject:@[@(point.x), @(point.y)]];
+                                                             }
+                                                             return [mutablePointsArray copy];
+                                                         }];
 }
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
