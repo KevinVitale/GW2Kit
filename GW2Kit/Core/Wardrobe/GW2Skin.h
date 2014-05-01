873f134f44fb7f8c4d43190953474aa484f4ec38
Kevin Vitale <kevinvitale@gmail.com>
2014-05-01 16:49:48 -0400

    Adding 'GW2Skin' protocol (incomplete)

--- GW2Kit/Core/Wardrobe/GW2Skin.h
+++ GW2Kit/Core/Wardrobe/GW2Skin.h
@@ -8,29 +8,6 @@
 
 #import "GW2Object.h"
 
-#pragma mark - Icon File
-// -----------------------------------------------------------------------------
-//  GW2IconFile, Protocol
-// -----------------------------------------------------------------------------
-/**
- *  @protocol   GW2IconFile
- *  @discussion Represents an item's icon. Used with the rendering service.
- */
-@protocol GW2IconFile <NSObject>
-@required
-
-/**
- *  @property   iconFileID
- */
-@property (copy, nonatomic, readonly)   NSString *iconFileID;
-
-/**
- *  @property   iconFileSignature
- */
-@property (copy, nonatomic, readonly)   NSString *iconFileSignature;
-@end
-
-
 #pragma mark - Skin Type
 // -----------------------------------------------------------------------------
 //  GW2SkinType, Protocol
