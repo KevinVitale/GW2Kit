74916db5079217d30fdb1fbe4bf457af89e76b98
Kevin Vitale <kevinvitale@gmail.com>
2014-05-19 16:59:38 -0400

    Fixed ColorSpec,ItemSpec  file names

--- Tests/Tests/GW2ClientV1Spec_Colors.m
+++ Tests/Tests/GW2ClientV1Spec_Colors.m
@@ -8,15 +8,14 @@
 
 #import "GW2SharedSpec.h"
 
-SpecBegin(GW2ClientV1_Items)
-describe(@"client items", ^ {
+SpecBegin(GW2ClientV1_Colors)
+describe(@"client colors", ^ {
     id<GW2ClientV1> __block client;
     
     // -----------------------------------------------------------------------------
     //  before
     // -----------------------------------------------------------------------------
     before(^ {
-        setAsyncSpecTimeout(1000000.f);
         client = GW2ClientV1(nil);
         expect(client.preferredLanguage).to.equal(@"en");
     });
@@ -24,70 +23,24 @@ describe(@"client items", ^ {
     // -----------------------------------------------------------------------------
     //  fetches colors
     // -----------------------------------------------------------------------------
-    it(@"fetches items", ^AsyncBlock {
-        NSArray *itemIDs = ({
-            NSData *data = [NSData dataWithContentsOfURL:({
-                [NSURL URLWithString:@"https://api.guildwars2.com/v1/items.json"];
-            })];
-            [NSJSONSerialization JSONObjectWithData:data
-                                            options:0
-                                              error:nil][@"items"];
-        });
-        
-        RACSequence *sequence   = itemIDs.rac_sequence;
-        NSString *urlString     = @"https://api.guildwars2.com/v1/item_details.json?item_id=";
-        NSString *urlString2    = @"https://tradingpost-live.ncplatform.net/ws/search.json?ids=";
-        
-        RACSignal *signal = ({
-            [[[[[sequence signal] take:1] deliverOn:[RACScheduler scheduler]] delay:1]
-             flattenMap:^RACStream *(NSNumber *itemID) {
-                 return
-                 [RACSignal zip:
-                  @[({
-                     NSURL *url = [NSURL URLWithString:[urlString stringByAppendingString:itemID.stringValue]];
-                     [[NSURLConnection rac_sendAsynchronousRequest:({
-                         [NSURLRequest requestWithURL:url];
-                     })] map:^id(RACTuple *tuple) {
-                         return [NSJSONSerialization JSONObjectWithData:tuple.last
-                                                                options:0
-                                                                  error:nil];
-                     }];
-                 })
-                    ,
-                    ({
-                     NSURL *url = [NSURL URLWithString:[urlString2 stringByAppendingString:itemID.stringValue]];
-                     [[NSURLConnection rac_sendAsynchronousRequest:({
-                         NSMutableURLRequest *request = [[NSURLRequest requestWithURL:url] mutableCopy];
-                         [request setValue:@"s=8C519C40-69A8-4769-AA7F-B3A745FE2820"
-                        forHTTPHeaderField:@"Cookie"];
-                         [request copy];
-                     })] map:^id(RACTuple *tuple) {
-                         return [NSJSONSerialization JSONObjectWithData:tuple.last
-                                                                options:0
-                                                                  error:nil];
-                     }];
-                 })
-                    ]];
-             }];
-        });
-        
-        [signal subscribeNext:^(id x) {
-            NSLog(@"%@", x);
-        }
-                        error:^(NSError *error) {
-                            expect(error).to.beNil();
-                            done();
-                        }
-                    completed:^{
-                        done();
-                    }];
+    it(@"fetches colors", ^AsyncBlock {
+        [[[client fetchColors] take:5]
+         subscribeNext:^(id colors) {
+             NSLog(@"%@", colors);
+         }
+         error:^(NSError *error) {
+             expect(error).to.beNil();
+             done();
+         }
+         completed:^{
+             done();
+         }];
     });
     
     // -----------------------------------------------------------------------------
     //  after
     // -----------------------------------------------------------------------------
     after(^ {
-        setAsyncSpecTimeout(10.f);
         client = nil;
     });
 });
--- Tests/Tests/GW2ClientV1Spec_Items.m
+++ Tests/Tests/GW2ClientV1Spec_Items.m
@@ -8,14 +8,15 @@
 
 #import "GW2SharedSpec.h"
 
-SpecBegin(GW2ClientV1_Colors)
-describe(@"client colors", ^ {
+SpecBegin(GW2ClientV1_Items)
+describe(@"client items", ^ {
     id<GW2ClientV1> __block client;
     
     // -----------------------------------------------------------------------------
     //  before
     // -----------------------------------------------------------------------------
     before(^ {
+        setAsyncSpecTimeout(1000000.f);
         client = GW2ClientV1(nil);
         expect(client.preferredLanguage).to.equal(@"en");
     });
@@ -23,24 +24,70 @@ describe(@"client colors", ^ {
     // -----------------------------------------------------------------------------
     //  fetches colors
     // -----------------------------------------------------------------------------
-    it(@"fetches colors", ^AsyncBlock {
-        [[[client fetchColors] take:5]
-         subscribeNext:^(id colors) {
-             NSLog(@"%@", colors);
-         }
-         error:^(NSError *error) {
-             expect(error).to.beNil();
-             done();
-         }
-         completed:^{
-             done();
-         }];
+    it(@"fetches items", ^AsyncBlock {
+        NSArray *itemIDs = ({
+            NSData *data = [NSData dataWithContentsOfURL:({
+                [NSURL URLWithString:@"https://api.guildwars2.com/v1/items.json"];
+            })];
+            [NSJSONSerialization JSONObjectWithData:data
+                                            options:0
+                                              error:nil][@"items"];
+        });
+        
+        RACSequence *sequence   = itemIDs.rac_sequence;
+        NSString *urlString     = @"https://api.guildwars2.com/v1/item_details.json?item_id=";
+        NSString *urlString2    = @"https://tradingpost-live.ncplatform.net/ws/search.json?ids=";
+        
+        RACSignal *signal = ({
+            [[[[[sequence signal] take:1] deliverOn:[RACScheduler scheduler]] delay:1]
+             flattenMap:^RACStream *(NSNumber *itemID) {
+                 return
+                 [RACSignal zip:
+                  @[({
+                     NSURL *url = [NSURL URLWithString:[urlString stringByAppendingString:itemID.stringValue]];
+                     [[NSURLConnection rac_sendAsynchronousRequest:({
+                         [NSURLRequest requestWithURL:url];
+                     })] map:^id(RACTuple *tuple) {
+                         return [NSJSONSerialization JSONObjectWithData:tuple.last
+                                                                options:0
+                                                                  error:nil];
+                     }];
+                 })
+                    ,
+                    ({
+                     NSURL *url = [NSURL URLWithString:[urlString2 stringByAppendingString:itemID.stringValue]];
+                     [[NSURLConnection rac_sendAsynchronousRequest:({
+                         NSMutableURLRequest *request = [[NSURLRequest requestWithURL:url] mutableCopy];
+                         [request setValue:@"s="
+                        forHTTPHeaderField:@"Cookie"];
+                         [request copy];
+                     })] map:^id(RACTuple *tuple) {
+                         return [NSJSONSerialization JSONObjectWithData:tuple.last
+                                                                options:0
+                                                                  error:nil];
+                     }];
+                 })
+                    ]];
+             }];
+        });
+        
+        [signal subscribeNext:^(id x) {
+            NSLog(@"%@", x);
+        }
+                        error:^(NSError *error) {
+                            expect(error).to.beNil();
+                            done();
+                        }
+                    completed:^{
+                        done();
+                    }];
     });
     
     // -----------------------------------------------------------------------------
     //  after
     // -----------------------------------------------------------------------------
     after(^ {
+        setAsyncSpecTimeout(10.f);
         client = nil;
     });
 });
