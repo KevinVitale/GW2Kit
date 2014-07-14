408143c996d18d2af0d58d7c80ba8aa55342e942
Kevin Vitale <kevinvitale@gmail.com>
2014-07-14 17:01:40 -0400

    Empty project w/framework targets

---
+++ GW2Kit/GW2Kit.h
@@ -0,0 +1,19 @@
+//
+//  GW2Kit-iOS.h
+//  GW2Kit-iOS
+//
+//  Created by Kevin Vitale on 7/14/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GW2Kit-iOS.
+FOUNDATION_EXPORT double GW2Kit_iOSVersionNumber;
+
+//! Project version string for GW2Kit-iOS.
+FOUNDATION_EXPORT const unsigned char GW2Kit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GW2Kit_iOS/PublicHeader.h>
+
+
